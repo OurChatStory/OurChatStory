48e378c62814d025868be0ac74203c4e491cb400
Anshul Agarwala <anshulagarwala@Anshuls-MacBook-Air.local>
2021-12-20 13:16:20 +0530

    test

--- src/script/parser.js
+++ src/script/parser.js
@@ -4,27 +4,20 @@
 //     return { x: hour, y: count };
 //   });
 //   arr.push(...arr.slice(0, n));
-//   console.log(arr.slice(n));
 //   return arr.slice(n);
 // };
 
-// module.exports.monthly_count_data = (data) => {
-//   return data.map(({ month, count }) => {
-//     return { x: month, y: count };
-//   });
-// };
+module.exports.active_time = (data) => {
+  return data < 12 ? data + " AM" : data - 12 + " PM";
+};
 
-// module.exports.active_time = (data) => {
-//   return data < 12 ? data + " AM" : data - 12 + " PM";
-// };
-
-// module.exports.active_time_type = (data) => {
-//   return data < 9 && data > 4
-//     ? "Looks like you both are Early Birds 🌅"
-//     : data > 20
-//     ? "Looks like you both are night owl🦉"
-//     : "";
-// };
+module.exports.active_time_type = (data) => {
+  return data < 9 && data > 4
+    ? "Looks like you both are Early Birds 🌅"
+    : data > 20
+    ? "Looks like you both are night owl🦉"
+    : "";
+};
 // module.exports.months = {
 //   Jan: "January",
 //   Feb: "February",
