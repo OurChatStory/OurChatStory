b4e85ae12a7cb6868c97fb2ce0fec5a074026586
Anshul Agarwala <anshulagarwala@Anshuls-MacBook-Air.local>
2021-12-22 12:46:16 +0530

    pwa fix

--- public/sw.js
+++ public/sw.js
@@ -56,3 +56,17 @@ self.addEventListener("fetch", function (event) {
     );
   }
 });
+// self.addEventListener('fetch', event => {
+//     const url = new URL(event.request.url);
+//     // If this is an incoming POST request for the
+//     // registered "action" URL, respond to it.
+//     if (event.request.method === 'POST' &&
+//         url.pathname === '/bookmark') {
+//       event.respondWith((async () => {
+//         const formData = await event.request.formData();
+//         const link = formData.get('link') || '';
+//         const responseUrl = await saveBookmark(link);
+//         return Response.redirect(responseUrl, 303);
+//       })());
+//     }
+//   });
