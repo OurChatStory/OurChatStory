be17f1611cbd0e2faa0fcbcc4298268099145431
divit-agrawal <divitagrawal@gmail.com>
2022-12-03 22:17:32 +0530

    fix. design

--- components/charts/GridStats.js
+++ components/charts/GridStats.js
@@ -1,4 +1,12 @@
-import { Heading, Box, Text, Grid, GridItem ,VStack, Center} from "@chakra-ui/react";
+import {
+  Heading,
+  Box,
+  Text,
+  Grid,
+  GridItem,
+  VStack,
+  Center,
+} from "@chakra-ui/react";
 const parser = require("../../script/parser");
 
 const GridStats = ({ drawData }) => {
@@ -7,9 +15,9 @@ const GridStats = ({ drawData }) => {
       spacing="1.5rem"
       align="center"
       justify="center"
-      // bgImage="static/bg3.png"
-      // backgroundPosition="center"
-      // backgroundRepeat="no-repeat"
+      bgImage="static/blur/v2bg2.jpg"
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
       // backgroundSize="120%"
       // p="2rem"
       // height="100vh"
@@ -18,46 +26,90 @@ const GridStats = ({ drawData }) => {
       h="78vh"
       bgColor="primary.800"
       borderRadius="1rem"
-      border="1px solid #f3c5b7"
+      border="2px solid #f78901"
       p="1rem"
     >
       {/* <Heading size="md" color="black" textAlign="center">
         An Emoji is worth thousand words
       </Heading> */}
 
-
-        <Grid
-          templateRows="repeat(2, 1fr)"
-          templateColumns="repeat(4, 1fr)"
-          gap={7}
+      <Grid
+        templateRows="repeat(2, 1fr)"
+        templateColumns="repeat(4, 1fr)"
+        gap={7}
+      >
+        <GridItem
+          colSpan={2}
+          border="1px solid #ffffff50"
+          p="1rem"
+          borderRadius={"6px"}
         >
-          <GridItem colSpan={2} border="1px solid #ffffff50" p="1rem" borderRadius={"6px"}>
-            <Center>
-              <VStack>
-            <Text fontSize={["md","md","lg"]} fontWeight="700">Minutes Involved</Text>
-            <Text fontSize={["5xl","7xl","8xl"]} fontWeight="800">{drawData.total_no_of_minutes}</Text>
-            <Text fontSize={["lg","sm","xl"]} fontWeight="200">get a life guys</Text>
-            </VStack></Center>
-          </GridItem>
-          <GridItem colSpan={2} border="1px solid #ffffff50" p="1rem" borderRadius={"6px"}>
-          <Text fontSize={["lg","md","md"]} fontWeight="700">That in hour is</Text>
-            <Text fontSize={["5xl","7xl","8xl"]} fontWeight="800">{parseInt((drawData.total_no_of_minutes)/60)}</Text>
-            <Text fontSize={["lg","sm","sm"]} fontWeight="200">get a life guys</Text>          </GridItem>
-          <GridItem colSpan={2} border="1px solid #ffffff50" p="1rem" borderRadius={"6px"}>
-          <Text fontSize={["sm","md","md"]} fontWeight="700">Days Involved</Text>
-            <Text fontSize={["5xl","7xl","8xl"]} fontWeight="800">
-              {parser.no_of_days_talked(drawData.no_talk_string)}
+          <Center>
+            <VStack>
+              <Text fontSize={["md", "md", "lg"]} fontWeight="700">
+                Minutes Involved
               </Text>
-            <Text fontSize={["sm","sm","sm"]} fontWeight="200">Thats more than couples in a relationship</Text>          </GridItem>
-          <GridItem colSpan={2} border="1px solid #ffffff50" p="1rem" borderRadius={"6px"}>
-          <Text fontSize={["sm","md","md"]} fontWeight="700">Placeholder</Text>
-            <Text fontSize={["5xl","7xl","8xl"]} fontWeight="800">2</Text>
-            <Text fontSize={["sm","sm","sm"]} fontWeight="200">get a life guys</Text>          </GridItem>
-        </Grid>
-        {/* <Heading size="md" color="black" textAlign="center" p="1rem">
+              <Text fontSize={["5xl", "7xl", "8xl"]} fontWeight="800">
+                {drawData.total_no_of_minutes}
+              </Text>
+              <Text fontSize={["lg", "sm", "xl"]} fontWeight="200">
+                get a life guys
+              </Text>
+            </VStack>
+          </Center>
+        </GridItem>
+        <GridItem
+          colSpan={2}
+          border="1px solid #ffffff50"
+          p="1rem"
+          borderRadius={"6px"}
+        >
+          <Text fontSize={["lg", "md", "md"]} fontWeight="700">
+            That in hour is
+          </Text>
+          <Text fontSize={["5xl", "7xl", "8xl"]} fontWeight="800">
+            {parseInt(drawData.total_no_of_minutes / 60)}
+          </Text>
+          <Text fontSize={["lg", "sm", "sm"]} fontWeight="200">
+            get a life guys
+          </Text>{" "}
+        </GridItem>
+        <GridItem
+          colSpan={2}
+          border="1px solid #ffffff50"
+          p="1rem"
+          borderRadius={"6px"}
+        >
+          <Text fontSize={["sm", "md", "md"]} fontWeight="700">
+            Days Involved
+          </Text>
+          <Text fontSize={["5xl", "7xl", "8xl"]} fontWeight="800">
+            {parser.no_of_days_talked(drawData.no_talk_string)}
+          </Text>
+          <Text fontSize={["sm", "sm", "sm"]} fontWeight="200">
+            Thats more than couples in a relationship
+          </Text>{" "}
+        </GridItem>
+        <GridItem
+          colSpan={2}
+          border="1px solid #ffffff50"
+          p="1rem"
+          borderRadius={"6px"}
+        >
+          <Text fontSize={["sm", "md", "md"]} fontWeight="700">
+            Placeholder
+          </Text>
+          <Text fontSize={["5xl", "7xl", "8xl"]} fontWeight="800">
+            2
+          </Text>
+          <Text fontSize={["sm", "sm", "sm"]} fontWeight="200">
+            get a life guys
+          </Text>{" "}
+        </GridItem>
+      </Grid>
+      {/* <Heading size="md" color="black" textAlign="center" p="1rem">
           So these are our most used Emojis
         </Heading> */}
-
     </VStack>
   );
 };
