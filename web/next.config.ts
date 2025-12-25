111b5218b5e3a2c415bfdf177a0e7ad8ddeee0e1
Yajat Malhotra <68477362+iamyajat@users.noreply.github.com>
2025-12-26 00:00:00 +0530

    refactor: remove Cloudflare configuration and related dependencies

--- web/next.config.ts
+++ web/next.config.ts
@@ -14,8 +14,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
-// Enable calling `getCloudflareContext()` in `next dev`.
-// See https://opennext.js.org/cloudflare/bindings#local-access-to-bindings.
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-initOpenNextCloudflareForDev();
